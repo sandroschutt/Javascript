97b7a8e16c12db26fa0258a9d465e95bf5f76003
Sandro Schutt <schuttsandro@outlook.com>
2022-03-10 12:36:51 -0300

    ADD 'exercicios' FOLDER

---
+++ exercicios/meu-script.js
@@ -0,0 +1,24 @@
+// alert('Olá! Seja bem-vindo ao curso!')
+
+
+/*
+	selecionar um elemento no DOM
+	atualizar o valor desse elemento com uma string
+*/ 
+// document.getElementById('nome').value = 'Oi'
+
+//string
+var texto = 'Curso de JavaScript'
+
+//number (int/float)
+var numeroInteiro = -7
+var numeroFracionado = 0.75
+
+//boolean
+var teste = true //true -> verdadeiro  false -> falso
+
+alert(numeroFracionado)//abrir um diálogo
+
+document.write(texto)//escreve no html
+
+console.log(numeroInteiro)//registra atividade no console
