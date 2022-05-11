edd13c9aa67e1572ab4e689b6595908889685e4a
Sandro Schutt Jr <schuttsandro@outlook.com>
2022-05-11 19:40:39 -0300

    removed unused variables p1, p2, p3, pn...

--- jogo-da-velha/jogo-da-velha.js
+++ jogo-da-velha/jogo-da-velha.js
@@ -1,16 +1,6 @@
 let player1 = "X";
 let player2 = "O";
 
-let p1 = document.getElementById("p1");
-let p2 = document.getElementById("p2");
-let p3 = document.getElementById("p3");
-let p4 = document.getElementById("p4");
-let p5 = document.getElementById("p5");
-let p6 = document.getElementById("p6");
-let p7 = document.getElementById("p7");
-let p8 = document.getElementById("p8");
-let p9 = document.getElementById("p9");
-
 let lastPlay = player2;
 
 function setPosition(event) {
